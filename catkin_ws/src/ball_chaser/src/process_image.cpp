f1daf6bd8995e7660bb75957c635df9f1c34e8ac
Matt Cotton <mattcotton20@hotmail.co.uk>
2020-08-09 18:25:08 +0000

    Ball tracking improved: targets centre of ball and robot stops once near

--- catkin_ws/src/ball_chaser/src/process_image.cpp
+++ catkin_ws/src/ball_chaser/src/process_image.cpp
@@ -18,42 +18,69 @@ void drive_robot(float lin_x, float ang_z)
         ROS_ERROR("Failed to call service /ball_chaser/command_robot");
 }
 
+// This function calculates the average value of a given vector of integers
+int vector_average(std::vector<int> v)
+{
+    int num_items = v.size();
+    int total = 0;
+
+    for(int i = 0; i < num_items; ++i)
+        total += v.at(i);
+    
+    return total / num_items;
+}
+
 // This callback function continuously executes and reads the image data
 void process_image_callback(const sensor_msgs::Image img)
 {
     // Tuneable settings
     const float turn_speed = 0.1;
     const float drive_speed = 0.1;
+    const float closeness_limit = 0.3;
     const int left_threshold = img.width / 3;
     const int right_threshold = img.width / 3 * 2;
 
     // Other variable declarations
     const int pixel_count = img.height * img.width;
-    const int white_pixel = 255;
+    const int white = 255;
     bool ball_found = false;
     int ball_pos;
+    std::vector<int> white_pixels;
 
     // Loop through every pixel in the image
-    for (int i = 0; i < pixel_count; ++i) {
-        if (img.data[3*i]   == white_pixel and
-            img.data[3*i+1] == white_pixel and
-            img.data[3*i+2] == white_pixel) {
-
-            ball_found = true; // If found, log it and stop searching
-	    ball_pos = i % img.width;
-	    break;
+    for (int i = 0; i < pixel_count; ++i)
+    {
+	// Test for white pixel
+        if (img.data[3*i]   == white and  
+            img.data[3*i+1] == white and
+            img.data[3*i+2] == white)
+        {
+	    // If white, flag it and store x-position of pixel in list
+            ball_found = true;
+            white_pixels.push_back(i % img.width);
         }
     }
 
-    if (!ball_found) // ball not in image - stop robot
-        drive_robot(0, 0);
-    else {
-        if (ball_pos < left_threshold) // ball in left sector - turn left
+    if (!ball_found)  // ball not in image - Spin anti-clockwise looking for it
+        drive_robot(0, 5*turn_speed);
+    else 
+    {
+	// Calculate the centre x-position of the ball
+        int ball_pos = vector_average(white_pixels); 
+
+        if (ball_pos < left_threshold)  // ball in left sector - turn left
 	    drive_robot(0, turn_speed);
-        else if (ball_pos < right_threshold) // ball in middle sector - drive forward
-	    drive_robot(drive_speed, 0);
-	else
-	    drive_robot(0, -turn_speed); // ball in right sector - turn right
+        else if (ball_pos > right_threshold)  // ball in right sector - turn right
+	    drive_robot(0, -turn_speed);
+	else  // Ball is straight ahead
+        {
+	    // If ball is close enough, stay put
+	    float ratio = (float)white_pixels.size() / (float)pixel_count;
+	    if (ratio > closeness_limit)
+	        drive_robot(0, 0);
+	    else
+	        drive_robot(drive_speed, 0);  // Otherwise drive towards it
+        }
     }
 }
 
